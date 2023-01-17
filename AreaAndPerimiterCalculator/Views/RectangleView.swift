53a713f1d2ce3b49dd63660cc0ca629a38a73f87
Alistair Fraser <afraser@lcs.on.ca>
2023-01-17 12:05:04 -0500

    added static slider and connected them to the length and width properties

--- AreaAndPerimiterCalculator/Views/RectangleView.swift
+++ AreaAndPerimiterCalculator/Views/RectangleView.swift
@@ -8,8 +8,67 @@
 import SwiftUI
 
 struct RectangleView: View {
+    
+    //MARK: Stored Properties
+    //@State is a property weapper
+    //Telling SwiftUI to "watch" these properties for changes
+    //Update the user interface when they change
+    @State  var length: Double = 70
+    @State var width: Double = 25
+    
+    //MARK: Computed Properties
+    var area: Double {
+        return length * width
+    }
+    //UI
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                Image("Rectangle")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 250)
+                Spacer()
+            }
+            
+            //Length
+            Text("Length")
+                .font(.title2)
+                .bold()
+            //Use string interpolation \() to display within
+            Text("\(length)")
+            
+            //Slider to control length
+            Slider(value: Binding.constant(length),
+                   in: 0...100,
+                   label: { Text("Length")},
+                   minimumValueLabel: { Text("0")},
+                   maximumValueLabel: { Text("100")})
+            
+            //Width
+            Text("Width")
+                .font(.title2)
+                .bold()
+            //Use string interpolation \() to display within
+            Text("\(width)")
+            
+            //Slider to control width
+            Slider(value: Binding.constant(width),
+                   in: 0...100,
+                   label: { Text("width")},
+                   minimumValueLabel: { Text("0")},
+                   maximumValueLabel: { Text("100")})
+            
+            //Area
+            Text("Area")
+                .font(.title2)
+                .bold()
+            //Use string interpolation \() to display within
+            Text("\(area)")
+            
+            
+            Spacer()
+        }
     }
 }
 
