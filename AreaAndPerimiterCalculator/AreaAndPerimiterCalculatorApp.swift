702f5b1d510b1da9e591a0adf5862d7dcb095944
Alistair Fraser <afraser@lcs.on.ca>
2023-01-17 11:36:29 -0500

    Initial Commit

---
+++ AreaAndPerimiterCalculator/AreaAndPerimiterCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AreaAndPerimiterCalculatorApp.swift
+//  AreaAndPerimiterCalculator
+//
+//  Created by Alistair Fraser on 2023-01-17.
+//
+
+import SwiftUI
+
+@main
+struct AreaAndPerimiterCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
